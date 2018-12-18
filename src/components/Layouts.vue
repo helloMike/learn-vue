cae57858cc9ef51479ade14a5ec244b204bc30ad
axing <759837320@qq.com>
2018-12-18 17:08:22 +0800

    增加了一些基础样式，以及layouts.vue这个页面

---
+++ src/components/Layouts.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <section class="menu">
+
+    </section>
+    <section class="content-container"></section>
+  </div>
+</template>
+<script></script>
+<style lang="less">
+
+  @import '../common/style/layout.less';
+</style>
